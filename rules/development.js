d14013fe703eeb984cf9041438f82fdfda96878e
Tijn Kersjes <tijn@divbyzero.nl>
2016-01-27 12:10:09 +0100

    Added preset for development tools

---
+++ rules/development.js
@@ -0,0 +1,26 @@
+module.exports = {
+    rules: {
+
+        // disable rules
+        "no-multiple-empty-lines": 0,
+        "no-debugger": 0,
+        "no-console": 0,
+
+        // relax rules
+        "prefer-arrow-callback": 1,
+        "camelcase": 1,
+        "require-jsdoc": 1,
+        "no-lone-blocks": 1,
+        "no-use-before-define": 1,
+        "no-lonely-if": 1,
+        "no-else-return": 1,
+        "no-loop-func": 1,
+        "no-unused-expressions": [1, { allowTernary: true, allowShortCircuit: true }],
+        "no-sequences": 1,
+        "callback-return": [1, ["callback", "cb", "next"]],
+        "comma-dangle": 1,
+        "no-empty": 1,
+        "no-octal": 1,
+        "no-unreachable": 1
+    }
+};
