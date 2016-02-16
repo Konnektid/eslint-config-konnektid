efafe852acf0d450f2a5a59606b812fc3e85936d
Tijn Kersjes <tijn@divbyzero.nl>
2016-02-16 13:53:03 +0100

    Removed Node environment from ES5 preset

--- es5.js
+++ es5.js
@@ -1,6 +1,5 @@
 module.exports = {
     extends: [
-        "eslint-config-konnektid/rules/node",
         "eslint-config-konnektid/rules/recommended",
         "eslint-config-konnektid/rules/bestpractices",
         "eslint-config-konnektid/rules/readability",
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
     extends: [
         "eslint-config-konnektid/es5",
+        "eslint-config-konnektid/rules/node",
         "eslint-config-konnektid/rules/es6"
     ]
 };
