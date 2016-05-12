9de8b5879a7f54e1c870f6a6a1680d4ff3c77f4d
siarheiv <siarhei.valenda@leverx.com>
2016-05-12 19:23:24 +0300

    Linux case sensitive issue fix

--- rules/base/index.js
+++ rules/base/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: [
         "eslint-config-konnektid/rules/base/preventability",
-        "eslint-config-konnektid/rules/base/bestpractices",
+        "eslint-config-konnektid/rules/base/bestPractices",
         "eslint-config-konnektid/rules/base/variability",
         "eslint-config-konnektid/rules/base/brevity",
         "eslint-config-konnektid/rules/base/breathability",
